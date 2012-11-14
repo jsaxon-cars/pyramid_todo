133f83cf0f9868441ff38c5b2e9438ce60f2ba44
James Saxon <jsaxon@cars.com>
2012-11-14 15:52:01 -0800

    Trying to figure out how to override jinja2 delimiters with something else.  Tricky?

--- myproject/__init__.py
+++ myproject/__init__.py
@@ -13,11 +13,20 @@ def main(global_config, **settings):
     DBSession.configure(bind=engine)
     Base.metadata.bind = engine
     config = Configurator(settings=settings)
+    jinja_settings = {
+        'block_start_string': '@@',
+        'block_end_string': '@@',
+        'variable_start_string': '@=',
+        'variable_end_string': '=@',
+        'autoescape': 'melbatoast'
+    }
+    config.registry.settings['pyramid_jinja2.config'] = jinja_settings
     config.include('pyramid_jinja2')
     config.include('myproject.fainit')
     config.add_static_view('static', 'static', cache_max_age=3600)
     config.add_route('home', '/')
     config.add_route('todo', '/todo')
     config.scan()
+    import pdb; pdb.set_trace()
     return config.make_wsgi_app()
 
